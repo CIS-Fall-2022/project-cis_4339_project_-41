f36583cf42a8b758cbedb885b88dbf73b8e92954
npeterq <85538898+npeterq@users.noreply.github.com>
2022-09-26 21:08:28 -0500

    Adding orgData route for API

---
+++ backend/routes/orgData.js
@@ -0,0 +1,52 @@
+const express = require("express"); 
+const router = express.Router(); 
+
+//importing data model schemas
+let { orgdata } = require("../models/models"); 
+
+//GET all entries
+router.get("/", (req, res, next) => { 
+    orgdata.find( 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
+//POST
+router.post("/", (req, res, next) => { 
+    orgdata.create( 
+        req.body,
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data); 
+            }
+        }
+    );
+    orgdata.createdAt;
+    orgdata.updatedAt;
+    orgdata.createdAt instanceof Date;
+});
+
+//PUT update (make sure req body doesn't have the id)
+router.put("/:id", (req, res, next) => { 
+    orgdata.findOneAndUpdate( 
+        { _id: req.params.id }, 
+        req.body,
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+module.exports = router;
