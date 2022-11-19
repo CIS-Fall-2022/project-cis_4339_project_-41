2d49155a99348b4cd8157d11361198f35f12805f
npeterq <85538898+npeterq@users.noreply.github.com>
2022-11-19 16:38:18 -0600

    Update eventForm.vue

--- frontend/src/components/eventForm.vue
+++ frontend/src/components/eventForm.vue
@@ -64,18 +64,6 @@
               />
             </label>
           </div>
-          <!-- form field -->
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">Organization ID</span>
-              <input
-                type="text"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="event.orgID"
-              />
-            </label>
-          </div>
 
           <div></div>
           <div></div>
@@ -197,6 +185,7 @@ export default {
           })
           .catch((error) => {
             console.log(error);
+            alert(error); // improved error handling by alerting on the frontend also
           });
       }
     },
