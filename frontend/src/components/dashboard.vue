40afb5c52141f567e479e516be13d3aae93a584d
npeterq <85538898+npeterq@users.noreply.github.com>
2022-11-19 16:58:11 -0600

    Update dashboard.vue

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -7,7 +7,7 @@
     <!-- Display Table -->
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
       <div class="ml-10">
-        <h2 class="text-2xl font-bold">List of Orgs</h2>
+        <h2 class="text-2xl font-bold">Table</h2>
         <h3 class="italic">Clients signed up for events that happened in the last 2 months.</h3>
       </div>
       <div class="flex flex-col col-span-2">
